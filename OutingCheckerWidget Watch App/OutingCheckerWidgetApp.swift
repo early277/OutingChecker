d9d8b278db334cb26ddcf4fa294520ba950d16fd
early277 <pyre-lender.5t@icloud.com>
2026-04-28 20:52:08 +0900

    Add files via upload

---
+++ OutingCheckerWidget Watch App/OutingCheckerWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  OutingCheckerWidgetApp.swift
+//  OutingCheckerWidget Watch App
+//
+//  Created by yusuke yoshida on 2026/04/28.
+//
+
+import SwiftUI
+
+@main
+struct OutingCheckerWidget_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
